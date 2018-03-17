1df2fb3881b95a0225ef51a125ba6a8a5df6f45c
Mark Stoecker <webmaster@desertwebdesigns.com>
2018-03-17 00:46:56 -0700

    Exercise 3

---
+++ LPTHW/ex3-1.py
@@ -0,0 +1,3 @@
+# Calculate the number of seconds in a year
+
+print "There are", 60 * 60 * 24 * 365, "seconds in a year"
---
+++ LPTHW/ex3.py
@@ -0,0 +1,42 @@
+# Print statement
+print "I will now count my chickens:"
+
+# Print "Hens" followed by calculation of 25+(30/6)
+print "Hens", 25 + 30 / 6
+
+# Print "Roosters" followed by calculation of 100-((25*3)%4)
+print "Roosters", 100 - 25 * 3 % 4
+
+# Print statement
+print "Now I will count the eggs:"
+
+# Calculate 3+2+1-5+(4%2)-(1/4)+6 (no floating-point math)
+# Rewrote to include floating-point math
+print 3 + 2 + 1 - 5 + 4 % 2 - 0.25 + 6
+
+# Print statement
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Print the boolean test result of "is 3+2 less than 5-7?"
+print 3 + 2 < 5 - 7
+
+# Print the question "What is 3+2?" followed by the result of 3+2
+print "What is 3 + 2?", 3 + 2
+
+# Print the question "What is 5-7?" followed by the result of 5-7
+print "What is 5 - 7?", 5 - 7
+
+# Print statement
+print "Oh, that's why it's False."
+
+# Print statement
+print "How about some more."
+
+# Print the question "Is it greater?" followed by the boolean test result of "is 5 greater than -2?"
+print "Is it greater?", 5 > -2
+
+# Print the question "Is it greater or equal?" followed by the boolean test result of "is 5 greater than or equal to -2?"
+print "Is it greater or equal?", 5 >= -2
+
+# Print the question "Is it less or equal?" followed by the boolean test results of "is 5 less than or equal to -2?"
+print "Is it less or equal?", 5 <= -2
